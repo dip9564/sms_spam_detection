6766265d0096b7af1d12742b60fab7721b4c4d7b
dip9564 <dipmondal9564@gmail.com>
2026-05-02 13:18:59 +0530

    slidebar

--- app.py
+++ app.py
@@ -18,6 +18,24 @@ except LookupError:
 ps = nltk.stem.porter.PorterStemmer()
 stop_words = set(stopwords.words('english'))
 
+with st.sidebar:
+    st.header("📱 SMS Spam Detector")
+
+    with st.expander("About this app"):
+        st.write("""
+                This SMS Spam Detection app uses Machine Learning 
+                to classify messages as Spam or Not Spam.
+                 
+                👉 Model: Logistic Regression  
+                 
+                👉 Vectorizer: CountVectorizer """)
+
+    with st.expander("Limitations"):
+        st.write("* Predictions may not be 100% accurate  \n\n* Model is still under improvement  \n\n* Avoid relying on this for critical decisions ")
+
+    with st.expander("Future work"):
+        st.write("- Better accuracy with TF-IDF  \n\n- Deep Learning models \n\n- Larger dataset training ")
+
 def text_transformed(text):
     text=text.lower() # convert text to lowercase
     text= nltk.word_tokenize(text) # tokenize the text
@@ -38,23 +56,26 @@ model = pickle.load(open('model.pkl', 'rb'))
 st.title("SMS Spam Detection")
 input_sms = st.text_area("Enter the SMS message:")
 
-
 if st.button("Predict"):
     # 1. preprocess 
     transformed_sms = text_transformed(input_sms)
     if transformed_sms.strip() == "":
-        st.warning("Please enter valid text")
+        st.error("Please enter valid text")
         st.stop()
     # 2. vectorize
     cv_sms = cv.transform([transformed_sms])
     # 3. predict
     result = model.predict(cv_sms)[0]
-
-    if result == 0:
-        st.warning("Spam")
-    else:
-        st.success("Not Spam")
-
     prob = model.predict_proba(cv_sms)
-    st.write(f"Spam probability: {prob[0][0]:.2f}")
-    st.write(f"Not Spam probability: {prob[0][1]:.2f}")
+
+    col1,col2=st.columns([2,1])
+    if result == 0:
+        col1.error("Spam detected")
+        col2.warning(f"Spam probability: {prob[0][0]:.2f} \n\n Not Spam probability: {prob[0][1]:.2f}")
+
+    else:
+        col1.success("Not Spam")
+        col2.info(f"Spam probability: {prob[0][0]:.2f} \n\n Not Spam probability: {prob[0][1]:.2f}")
+
+
+    
