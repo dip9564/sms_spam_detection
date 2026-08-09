d9ae8c5b7b0e61dd9720e836313a2208cd861873
dip9564 <dipmondal9564@gmail.com>
2026-08-10 01:33:44 +0530

    add fastapi

--- frontend.py
+++ frontend.py
@@ -7,7 +7,6 @@ page_bg_img = """
     background-image: url("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg.freepik.com%2Fphotos-gratuite%2Fabstrait-numerique-grille-fond-noir_53876-97647.jpg%3Fsemt%3Dais_hybrid%26w%3D740&f=1&nofb=1&ipt=3a551da015306bbddd270ba2ae800e19c0cd7a495531591dc2fac4df03aeca4c");
     background-size: cover;
 }
-
 [data-testid="stHeader"] {
 background-color: rgba(0, 0, 0, 0);
 }
